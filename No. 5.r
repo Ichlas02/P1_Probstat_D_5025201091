861937c33f2550a10e6391da28bd920df15a953e
Ichlas02 <88977654+Ichlas02@users.noreply.github.com>
2022-04-10 21:21:14 +0700

    Update No. 5.r

--- No. 5.r
+++ No. 5.r
@@ -14,6 +14,7 @@ set.seed(1)
 dexp(3)
 
 # 5b = Histogram dari Distribusi Exponensial untuk 10, 100, 1000 dan 10000 bilangan random
+set.seed(1)
 hist(rexp(10)) # Histogram distribusi eksponensial untuk 10 angka random
 hist(rexp(100)) # Histogram distribusi eksponensial untuk 100 angka random
 hist(rexp(1000)) # Histogram distribusi eksponensial untuk 1000 angka random
