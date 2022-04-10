010f35b2fa8ff73e73375173fd2754654d372c14
Ichlas02 <88977654+Ichlas02@users.noreply.github.com>
2022-04-10 20:59:09 +0700

    Update No. 3.r

--- No. 3.r
+++ No. 3.r
@@ -15,17 +15,18 @@ dpois(x, rata)
 # 3b = Simulasi kelahiran 6 bayi selama setahun
 set.seed(0)
 h <- hist(rpois(365, rata), plot=FALSE)
-cuts <- cut(h$breaks, c(6.0,6.0))
+cuts <- cut(h$breaks, c(5.9,6.0))
 plot(h, col=cuts)
 
 # Rumus untuk menghitung rata-rata lahirnya 6 bayi dalam setahun menggunakan poisson
+set.seed(0)
 mean(rpois(364, rata) == 6) 
 
 #untuk menghitung jumlah lahirnya 6 bayi per ari dalam setahun
+set.seed(0)
 length(which(rpois(365, rata) == 6))
 
-# 3c = berdasarkan hasil perhitungan pada poin a dan b. Dapat disimpulkan bahwa dalam setahun,
-#      rata-rata kemunculan 6 bayi per hari hampir sama besarnya dengan kedua rumus tersebut.
+# 3c = berdasarkan hasil perhitungan pada poin a dan b. Dapat disimpulkan bahwa dalam setahun, rata-rata kemunculan 6 bayi per hari hampir sama besarnya dengan kedua rumus tersebut.
 
 # 3d = Menghitung rata-rata dan varian
 # note = untuk distribusi poisson, nilai rata-rata dan varian memang dianggap sama
